e14cb046298e0b2fd58ecbde203d454f3369442f
Jonas Schäfer <j.wielicki@sotecware.net>
2019-06-19 15:53:48 +0200

    Monitoring, fixes, backups

--- templates/job.sh
+++ templates/job.sh
@@ -7,5 +7,8 @@ export BORG_REPO="{{ repo.user }}@{{ repo.host }}:{{ repo.path }}"
 export BORG_REMOTE_PATH="{{ repo.executable }}"
 {% endif %}
 export BORG_RSH="ssh -i /etc/borgbackup/repositories/{{ item.value.repository }}.key"
-export PRUNE_FLAGS='-P {{ item.value.archive_prefix }}- {% for interval, amount in item.value.prune.iteritems() %}--keep-{{ interval }} {{ amount }} {% endfor %}'
+export PRUNE_FLAGS='-P {{ item.value.archive_prefix }}- {% for interval, amount in item.value.prune.items() %}--keep-{{ interval }} {{ amount }} {% endfor %}'
 export CREATE_FLAGS='-x{% if item.value.compression | default(False) %} -C {{ item.value.compression }}{% endif %} ::{{ item.value.archive_prefix }}-{utcnow} {{ item.value.path }}'
+export JOB_PATH="{{ item.value.path }}"
+export JOB_NAME="{{ item.key }}"
+export JOB_REPO="{{ item.value.repository }}"
